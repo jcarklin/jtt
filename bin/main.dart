f986e59b2f49e03a5ca9a8d326595efe6d429631
jcarklin <jcarklin@gmail.com>
2019-12-16 20:53:25 +0200

    removed pick class, added enums

--- bin/main.dart
+++ bin/main.dart
@@ -1,61 +1,24 @@
-import 'package:jtt_commandline/src/project.dart';
 import 'package:jtt_commandline/src/tablet.dart';
 
 main(List<String> arguments) {
-  
-  Project firstProject = new Project();
-  firstProject.createDeck(10, 4);
-  print('Deck has been created');
-  print('======================');
-  firstProject.printDeck();
 
-  firstProject.deck.cards.forEach((tablet)=>tablet.threadTablet(0, Thread(0,'red')));
-  firstProject.deck.cards.forEach((tablet)=>tablet.threadTablet(1, Thread(1,'green')));
-  firstProject.deck.cards.forEach((tablet)=>tablet.threadTablet(2, Thread(2,'blue')));
-  firstProject.deck.cards.forEach((tablet)=>tablet.threadTablet(3, Thread(3,'yellow')));
-  print('First thread set to red');
-  print('======================');
-  firstProject.printDeck();
+  List<Thread> threads = List();
+  threads.add(Thread('Red')); 
+  threads.add(Thread('Green')); 
+  threads.add(Thread('Blue')); 
+  threads.add(Thread('Yellow'));
 
-  firstProject.deck.cards.forEach((tablet)=>tablet.prepareTurn(true));
-  print('Deck prepared for turning');
-  print('======================');
-  firstProject.printDeck();
+  Tablet tablet = Tablet(threads, true, true);
+  print(tablet);
+  tablet = Tablet(threads, true, false);
+  print(tablet);
+  tablet = Tablet(threads, false, true);
+  tablet.turnDirection=TurnDirection.FORWARD;
+  print(tablet);
 
-  firstProject.deck.turnCards();
-  print('Deck Turned Forward');
-  print('======================');
-  firstProject.printDeck();
-
-  firstProject.deck.cards.forEach((tablet)=>tablet.prepareTurn(true));
-  print('Deck prepared for turning');
-  print('======================');
-  firstProject.printDeck();
-
-  firstProject.deck.turnCards();
-  print('Deck Turned Forward');
-  print('======================');
-  firstProject.printDeck();
-
-  firstProject.deck.cards.forEach((tablet)=>tablet.prepareTurn(false));
-  print('Deck prepared for turning');
-  print('======================');
-  firstProject.printDeck();
-
-  firstProject.deck.turnCards();
-  print('Deck Turned Backwards');
-  print('======================');
-  firstProject.printDeck();
-
-  firstProject.deck.cards.forEach((tablet)=>tablet.prepareTurn(false, sTwist: false));
-  print('Deck prepared for turning (twisted)');
-  print('======================');
-  firstProject.printDeck();
-
-  firstProject.deck.turnCards();
-  print('Deck Turned Backwards');
-  print('======================');
-  firstProject.printDeck();
+  tablet = Tablet(threads, false, false);
+  tablet.turnDirection=TurnDirection.BACKWARD;
+  print(tablet);
 }
 
 
--- lib/src/tablet.dart
+++ lib/src/tablet.dart
@@ -1,58 +1,54 @@
+import 'dart:collection';
+
+enum HoleIndex{
+  BACK_TOP,BACK_BOTTOM,FRONT_BOTTOM,FRONT_TOP
+}
+enum TurnDirection {
+  FORWARD,BACKWARD,NONE
+}
+
 class Tablet {
-  List<Thread> _threads; //limited to number of holes, immutable 
-  List<Pick> _picks = List();
-  Pick _nextPick;
-  
-  Tablet(bool sTwist, int numHoles) {
-      _threads = List.generate(numHoles, (holeIndex)=>Thread(holeIndex,null) );
-      _picks.add(Pick(sTwist, null, _threads.map((thread)=> thread.cardIndex).toList(), this));
+  UnmodifiableListView<Thread> _threads; //limited to number of holes, immutable 
+  bool _clockwise=true;
+  bool _sTwist; //S or Z 
+  TurnDirection _turnedDir;//Forwards or Backwards
+
+  Tablet(List<Thread> threads, this._sTwist, this._clockwise) {
+    this._threads = new UnmodifiableListView(threads);
   }
 
-  threadTablet(int threadIndex, Thread thread) {
-      _threads[threadIndex] = thread;
+  bool get clockwise => _clockwise;
+  set twist(bool twist) {this._sTwist = twist;}
+  set turnDirection(TurnDirection direction) {this._turnedDir = direction;}
+
+  int getIndexInRange(index, length) {
+    var trim = index % length;
+    var nonNegative = trim + length;
+    return nonNegative % length;
   }
 
-  turnTablet() {
-    _picks.add(_nextPick);
-    _nextPick = null;  
-  }
-
-  bool isReadyToTurn() {
-    return _nextPick!=null;
-  }
-
-  prepareTurn(bool isForward, {bool sTwist}) {
-    if (!isReadyToTurn()) {
-      List<int> nextState = List();
-      if (isForward) {
-        nextState.addAll(_picks.last._threadsState.sublist(0,_threads.length-1));
-        nextState.insert(0,_picks.last._threadsState[_threads.length-1]);
-      } else {
-        nextState.addAll(_picks.last._threadsState.sublist(1));
-        nextState.add(_picks.last._threadsState[0]);      
-      }
-      _nextPick = Pick(sTwist==null?_picks.last.sTwist:sTwist, isForward, nextState, this);
-    }
+  String getAngle() {
+    return (_sTwist &&_turnedDir==TurnDirection.FORWARD)?'/':'\\';
   }
 
   @override
   String toString() {
-    Pick printPick = _picks.last;
-    String to = (isReadyToTurn()?'Ready to turn':'Not ready for turning') + '\t'+ printPick.toString();
-    printPick._threadsState.forEach((threadIndex)=> to += _threads[threadIndex].toString()+', ');
-    to += '\n';
-    return to;
+    String colours = _clockwise?'Clockwise':'Anticlockwise';
+    int index;
+    
+    for(int i = 0; i < _threads.length; i++) {
+      index = getIndexInRange(_clockwise?0 + i:0-i, _threads.length);
+      colours += '\n$i: '+_threads[index].colour;
+    }
+     
+    return ((_sTwist?'S - ':'Z - ')+ getAngle() + colours);
   }
-  
 }
   
 class Thread {
-  int _indexOnCard;
   String _colour;
 
-  Thread(this._indexOnCard, this._colour);
-
-  int get cardIndex => _indexOnCard;
+  Thread(this._colour);
 
   String get colour => _colour;
 
@@ -62,24 +58,7 @@ class Thread {
 
   @override
   String toString() {
-    return ('['+(colour==null?'':colour)+']');
+    return _colour;
   }
 
-}
-
-class Pick {
-  bool _sTwist; //S or Z 
-  bool _turnedForward;//Forwards or Backwards
-  List<int> _threadsState;
-  Tablet _parentCard;
-
-  Pick(this._sTwist, this._turnedForward, this._threadsState, this._parentCard);
-
-  get sTwist => _sTwist;
-
-  @override
-  String toString() {
-    String to = ('Twist: ' + (_sTwist?'- S - ':'- Z - '));
-    return to + (_turnedForward==null?'':((_sTwist==_turnedForward)?' / ':' \\ '));
-  }
 }
